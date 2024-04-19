83a6055a9df85c693f649c9c45d17320e9c8d3cf
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-04-19 17:48:38 +0530

    First commit

--- src/Basics/Even_odd.java
+++ src/Basics/Even_odd.java
@@ -1,4 +1,17 @@
 package Basics;
 
 public class Even_odd {
+    public static void main(String[] args) {
+        int number = 10;
+
+        //Method 1
+//        if (number % 2 == 0)
+//            System.out.println(number + " is Even");
+//        else
+//            System.out.println(number + " is odd");
+
+        //Method 2
+        String status = number % 2 == 0 ? " is Even" : " is Odd";
+        System.out.println (number + status);
+    }
 }
