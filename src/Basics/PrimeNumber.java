bfdcea3d0e4d23de80281b9d8539020e3a139f5e
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-04-21 23:59:22 +0530

    First commit

---
+++ src/Basics/PrimeNumber.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int number=15;
+        isPrime(number);
+    }
+    public static void isPrime(int n){
+        int count =0;
+        if(n>2){
+            System.out.println("Not Prime");
+            return;
+        }
+        for (int i=1;i<=n;i++){
+            if(n%i==0)
+                count++;
+        }
+        if (count > 2)
+            System.out.println ("The given is number " + n + " is not prime");
+
+        else
+            System.out.println ("The given is number " + n + " is prime");
+    }
+}
