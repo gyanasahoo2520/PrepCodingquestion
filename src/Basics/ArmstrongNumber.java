e8a10f2ad8342969eee8f653c6113922b9f20fb6
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-04-23 17:37:24 +0530

    First commit

---
+++ src/Basics/ArmstrongNumber.java
@@ -0,0 +1,30 @@
+package Basics;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int num=121;
+        int len=Len(num);
+        if(armstrong(num,len))
+            System.out.println(num + " is Armstrong");
+        else
+            System.out.println(num + " is not Armstrong");
+    }
+    static int Len(int n){
+        int len=0;
+        while(n>0){
+            len++;
+            n=n/10;
+        }
+        return len;
+    }
+    static boolean armstrong(int n,int l){
+        int sum=0,temp,digit;
+        temp=n;
+        while (temp>0){
+            digit=temp%10;
+            sum=sum+(int)Math.pow(digit,l);
+            temp=temp/10;
+        }
+        return n==sum;
+    }
+}
