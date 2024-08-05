8935e66306a2da42cff3307bb24b4d624b80199b
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-08-05 20:25:02 +0530

    basics

---
+++ src/Basics/FactorOfNums.java
@@ -0,0 +1,32 @@
+package Basics;
+
+public class FactorOfNums {
+    public static void main(String[] args) {
+//        int num = 10;
+//
+//        System.out.println( "Factors of " + num + " are " );
+//
+//        // finding and printing factors b/w 1 to num
+//        for(int i = 1; i <= num; i++)
+//        {
+//            if(num % i == 0)
+//                System.out.println(i + " ");
+//        }
+        int num = 100;
+        getFactors (num);
+    }
+
+    static void getFactors (int num)
+    {
+
+        for (int i = 1; i <= (num/2); i++)
+        {
+            if (num % i == 0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+        // print thenumber itself too
+        System.out.print(num);
+    }
+}
---
+++ src/Basics/PerfectNum.java
@@ -0,0 +1,43 @@
+package Basics;
+
+public class PerfectNum {
+    public static void main(String[] args) {
+//        int n = 28, sum = 0;
+//
+//        for (int i = 1; i < n; i++)
+//        {
+//            if (n % i == 0)
+//                sum = sum + i;
+//        }
+//
+//        if (sum == n)
+//            System.out.println (n + " Is a perfect number");
+//        else
+//            System.out.println (n + " Is not a perfect number");
+
+        int num = 28;
+
+        if (isPerfect(num))
+            System.out.println (num + " Is a perfect number");
+        else
+            System.out.println (num + " Is not a perfect number");
+
+    }
+
+    private static boolean isPerfect(int num) {
+        int sum = 0;
+
+        // all divisors of num(excluding itself) can be found before num/2
+        // remember put = sign as for even numbers like 28
+        // half of it i.e. 14 would be divisor too e
+        for (int i = 1; i <= num/2; i++){
+            if (num % i == 0)
+                sum = sum + i;
+        }
+
+        if (sum == num)
+            return true;
+
+        return false;
+    }
+}
