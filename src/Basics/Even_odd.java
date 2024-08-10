263eb83c888078b2cd7c32f2a29bafaabb80ddde
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-08-10 08:08:56 +0530

    basics

--- src/Basics/Even_odd.java
+++ src/Basics/Even_odd.java
@@ -1,5 +1,7 @@
 package Basics;
 
+import javax.swing.*;
+
 public class Even_odd {
     public static void main(String[] args) {
         int number = 10;
@@ -11,7 +13,23 @@ public class Even_odd {
 //            System.out.println(number + " is odd");
 
         //Method 2
-        String status = number % 2 == 0 ? " is Even" : " is Odd";
-        System.out.println (number + status);
+//        String status = number % 2 == 0 ? " is Even" : " is Odd";
+//        System.out.println (number + status);
+
+//        method 3 without % operator
+//        if(isEven(number)==0)
+//            System.out.println("Even");
+//        else
+//            System.out.println("Number is Odd");
+
+//        method 4 without % operator
+        if((number/2)*2==number)
+            System.out.println("Even");
+        else
+            System.out.println("odd");
+    }
+
+    private static int isEven(int num) {
+        return (num & 1);
     }
 }
